b27056ed5133179cc418f41d1fbdfb5431044752
Dragan89 <Archaic.dragan@gmail.com>
2015-07-27 17:53:16 +0200

    Added dependency injection with Unity

---
+++ ResourcesProject/Dependency/DependencyFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ResourcesProject.Dependency
+{
+    public static class DependencyFactory
+    {
+        private static IUnityContainer _container;
+
+        public static T Get<T>()
+        {
+            return _container.Resolve<T>();
+        }
+
+        public static T Get<T>(string name)
+        {
+            return _container.Resolve<T>(name);
+        }
+
+        private static void Init()
+        {
+            _container = new UnityContainer();
+            UnityBootstrapper.RegisterTypes(_container);
+        }
+
+    }
+}
--- ResourcesProject/Global.asax.cs
+++ ResourcesProject/Global.asax.cs
@@ -16,6 +16,8 @@ namespace ResourcesProject
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //Setup Dependency Injection
+            UnityBootstrapper.Initialize();
         }
     }
 }
